dffebbde7f03d9dae48573d2098c1d04b7ef3828
Sam Aaron <samaaron@gmail.com>
2011-09-14 14:03:44 +0100

    bump version to 0.3.0

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject overtone/byte-spec "0.2.0-SNAPSHOT"
+(defproject overtone/byte-spec "0.3.0"
   :description "A DSL for reading and writing binary file formats in Clojure."
   :dependencies [[org.clojure/clojure "1.3.0-RC0"]])
