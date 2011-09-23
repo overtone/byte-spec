cee81dc8ced2771fafc04a4cbc34582a661649d5
Sam Aaron <samaaron@gmail.com>
2011-09-23 23:58:33 +0200

    Depend on Clojure 1.3.0 final

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject overtone/byte-spec "0.3.0"
+(defproject overtone/byte-spec "0.3.1"
   :description "A DSL for reading and writing binary file formats in Clojure."
-  :dependencies [[org.clojure/clojure "1.3.0-RC0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
